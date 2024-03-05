80e7e48a7da85f4931c4d4904909467ceaa96aa2
robert_bhanu <robertbhanuprasad0108@gmil.com>
2024-03-05 19:48:16 +0530

    fix:clean up to margin genres

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react"
+import { Button, HStack, Heading, Image, List, ListItem, Spinner } from "@chakra-ui/react"
 import useGenres, { Genre } from "../hooks/useGenres"
 
 interface Props {
@@ -10,12 +10,16 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   if (isLoading) return <Spinner />
   if (error) return null
   return (
+    <>
+    <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
     <List>
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
-            <Image boxSize="32px" borderRadius={8} src={genre.image_background} />
+            <Image boxSize="32px" borderRadius={8} objectFit='cover' src={genre.image_background} />
             <Button
+              whiteSpace="normal"
+              textAlign="left"
               fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               fontSize="lg"
@@ -27,6 +31,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
         </ListItem>
       ))}
     </List>
+    </>
   )
 }
 
