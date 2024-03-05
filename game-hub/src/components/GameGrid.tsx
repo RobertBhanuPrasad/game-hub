2e5d8677d95c5cd02d807c1af5ef2f88d33e7368
robert_bhanu <robertbhanuprasad0108@gmil.com>
2024-03-05 19:51:41 +0530

    fix:game cards

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -7,25 +7,25 @@ import GameCardContainer from "./GameCardContainer"
 import { Platform } from "../hooks/useGames"
 import { GameQuery } from "../App"
 
-interface Props{
-  gameQuery:GameQuery
+interface Props {
+  gameQuery: GameQuery
 }
-const GameGrid = ({gameQuery}:Props) => {
+const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery)
   const skeletons = [1, 2, 3, 4, 5, 6]
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding="10px" spacing={3}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding="10px" spacing={6}>
         {isLoading &&
           skeletons.map((Skeleton) => (
-            <GameCardContainer key={Skeleton} >
+            <GameCardContainer key={Skeleton}>
               <GameCardSkeleton />
             </GameCardContainer>
           ))}
         {data.map((game) => (
-          <GameCardContainer key={game.id} >
+          <GameCardContainer key={game.id}>
             <GameCard game={game} />
           </GameCardContainer>
         ))}
