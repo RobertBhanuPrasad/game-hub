5763cfdd9cf308501ca8b8ad4a6e23714cb55120
robert_bhanu <robertbhanuprasad0108@gmil.com>
2024-03-05 19:02:44 +0530

    fix:search input

--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image} from '@chakra-ui/react';
 import game from '../assets/game.png';
 import ColorModeSwitch from './ColorModeSwitch';
+import SearchInput from './SearchInput';
 const NavBar = () => {
   return (
     <HStack justifyContent={'space-between'} padding='10px'>
         <Image src={game} boxSize='60px'/>
+        <SearchInput/>
         <ColorModeSwitch/>
     </HStack>
   )
